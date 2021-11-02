f89d1af0d3a0b51cd4ef35819d91b8a1c244bfb3
hassan alhassaini <hassandak1d@hotmail.com>
2021-11-02 06:54:09 +0300

    done

--- apis/products/db/dataBases.js
+++ apis/products/db/dataBases.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   const conn = await mongoose.connect(
-    "mongodb+srv://dinok1d:sayed@myfirstcluster.clyuy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "mongodb+srv://dinok1d:@myfirstcluster.clyuy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     {
       useUnifiedTopology: true,
       useNewUrlParser: true,
